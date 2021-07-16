292520f3546a95816aa9cb31402f1a7a23a26ec6
Summerstar2 <87510284+Summerstar2@users.noreply.github.com>
2021-07-16 19:58:53 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -20,27 +20,27 @@ else:
     time.sleep(0.7)
     print ("Now!")
     time.sleep(0.5)
-    print ("Question 1! \nWhat is the capital of India?")
+    print ("Question 1!\nWhat is the capital of India?")
     Ans1=str(input("Enter your answer - "))
     if Ans1=="New Delhi" or "new delhi":
         Score=Score+2
-    print ("Question 2! \nHow many continents are there in the world?")
+    print ("Question 2!\nHow many continents are there in the world?")
     Ans2=int(input("Enter your answer - "))
     if Ans2==7:
         Score=Score+2
-    print ("Question 3! \nWhich is the biggest ocean in the world?")
+    print ("Question 3!\nWhich is the biggest ocean in the world?")
     Ans3=str(input("Enter your answer - "))
     if Ans3=="Pacific Ocean" or "pacific ocean" or "Pacific ocean":
         Score=Score+2
-    print ("Question 4! \nWhich country was Alaska a part of in the 18th century? \nA)USA \nB)Russia \nC)Canada \nD)Denmark")
+    print ("Question 4!\nWhich country was Alaska a part of in the 18th century?\nA)USA\nB)Russia\nC)Canada\nD)Denmark")
     Ans4=str(input("Enter your answer - "))
     if Ans4=="Russia":
         Score=Score+2
-    print ("Question 5! \nWho is the current president of Russia?")
+    print ("Question 5!\nWho is the current president of Russia?")
     Ans5=str(input("Enter you answer - "))
     if Ans5=="Valdmir Putin" or "vladmir putin" or "putin" or "Putin":
         Score=Score+2
     time.sleep(0.5)
     print ("The quiz has now ended and your scores are...")
     time.sleep(1.0)
-    print ("You scored ", Score, "out of 10!")
+    print ("You scored", Score, "out of 10!")
